51fa78bbdc511347242224c30b6190322da68418
OSHIN <izkp44@gmail.com>
2023-03-14 21:06:59 +0700

    Added FirstPage

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import FirstPage from './pages/FirstPage'
+
 function App() {
-  return <div className="App"></div>
+  return (
+    <div className="font-poppins">
+      <FirstPage />
+    </div>
+  )
 }
 
 export default App
