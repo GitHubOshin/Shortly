830e2ea151ce5233e4f262f3ad88760910ef510f
OSHIN <izkp44@gmail.com>
2023-03-15 16:21:58 +0700

    Moved isMenuOpening from Header to here

--- src/pages/FirstPage.js
+++ src/pages/FirstPage.js
@@ -1,14 +1,21 @@
+import { useState } from 'react'
 import Header from '../components/firstpage/Header'
 import TwoItems from '../components/firstpage/TwoItems'
+import MenuMobile from '../components/firstpage/MenuMobile'
 
 function FirstPage() {
+  const [isMenuOpening, setIsMenuOpening] = useState(false)
+
   return (
-    <section className="">
-      <Header />
-      <TwoItems />
+    <section className="flex flex-col items-center">
+      <Header
+        isMenuOpening={isMenuOpening}
+        setIsMenuOpening={setIsMenuOpening}
+      />
+      {isMenuOpening === true ? <MenuMobile /> : null}
+      <TwoItems isMenuOpening={isMenuOpening} />
     </section>
   )
 }
 
 export default FirstPage
-
