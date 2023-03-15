7a7e43ec2909b7d32c4c17bd58ecbf7e42da3e34
OSHIN <izkp44@gmail.com>
2023-03-16 03:12:00 +0700

    Added SecondPage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import FirstPage from './pages/FirstPage'
 import LastPage from './pages/LastPage'
+import SecondPage from './pages/SecondPage'
 
 function App() {
   return (
     <div className="font-poppins min-w-[375px]">
       <FirstPage />
+      <SecondPage />
       <LastPage />
     </div>
   )
