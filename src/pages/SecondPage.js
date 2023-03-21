88f85be9007dfe3c7722e049f4f0126cee8ebf34
OSHIN <izkp44@gmail.com>
2023-03-21 22:55:47 +0700

    Deleted old shorten link component

--- src/pages/SecondPage.js
+++ src/pages/SecondPage.js
@@ -5,19 +5,6 @@ function SecondPage() {
   return (
     <section className="relative bg-[#F0F1F6] flex flex-col items-center px-6 pt-[84px]">
       <Input />
-      <div className="mt-6 w-[1110px] flex flex-col gap-4">
-        <div className="bg-white h-[72px] rounded-md px-8 flex justify-between items-center py-4">
-          <h1>Hellooooooooooo</h1>
-          <div className="h-full flex items-center gap-6">
-            <a href="/" className="text-cyan">
-              CopyCopyCopyCopyCopy
-            </a>
-            <button className="bg-cyan hover:bg-[#A7E2E3] w-[103px] h-full text-white font-bold rounded-md">
-              Copy
-            </button>
-          </div>
-        </div>
-      </div>
       <div className="text-center min-w-[289px] max-w-[515px] mobile:mb-[47px] desktop:mb-[57px] mt-20">
         <h1 className="font-bold mobile:text-2xl desktop:text-4xl mb-5">
           Advanced Statistics
