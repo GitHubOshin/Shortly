1772fd140fc796d05cdc1377a58473f90d2098a8
OSHIN <izkp44@gmail.com>
2023-03-23 01:42:23 +0700

    Update Input.js

--- src/components/secondpage/Input.js
+++ src/components/secondpage/Input.js
@@ -1,5 +1,16 @@
 function Input(props) {
-  const { inputLink, setInputLink } = props
+  const { inputLink, setInputLink, handleCreateShortenLink } = props
+
+  function handleShortenLink(e) {
+    e.preventDefault()
+    if (inputLink === '') {
+      console.log(' Please add a link')
+    } else {
+      setInputLink('')
+      handleCreateShortenLink(inputLink)
+    }
+  }
+
   return (
     <div className="absolute w-full top-[0px] flex justify-center items-center">
       <img
@@ -12,17 +23,20 @@ function Input(props) {
         alt="Background"
         src="images/backgrounds/bg-shorten-mobile.svg"
       />
-      <div className="absolute mobile:w-[336.98px] mobile:h-[182px] desktop:w-full desktop:h-fit flex mobile:flex-col desktop:flex-row justify-center items-center gap-[24px]">
+      <form className="absolute mobile:w-[336.98px] mobile:h-[182px] desktop:w-full desktop:h-fit flex mobile:flex-col desktop:flex-row justify-center items-center gap-[24px]">
         <input
           className="w-full mobile:h-[48px] mobile:w-[280px] desktop:w-[769px] desktop:h-[64px] rounded-lg mobile:px-3 desktop:px-[30px] text-xl"
           placeholder="Shorten a link here..."
           value={inputLink}
           onChange={(e) => setInputLink(e.target.value)}
         />
-        <button className="bg-cyan hover:bg-[#A7E2E3] text-white disabled:none mobile:h-[48px] desktop:h-[64px] mobile:w-[279px] desktop:w-[188px] rounded-lg text-xl font-bold">
+        <button
+          className="bg-cyan hover:bg-[#A7E2E3] text-white disabled:none mobile:h-[48px] desktop:h-[64px] mobile:w-[279px] desktop:w-[188px] rounded-lg text-xl font-bold"
+          onClick={handleShortenLink}
+        >
           Shorten It!
         </button>
-      </div>
+      </form>
     </div>
   )
 }
